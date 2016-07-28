eee3210f52667fefd7c02893c8bf7742a5671a60
masweetman <slosweetman@gmail.com>
2016-07-27 18:18:17 -0700

    test1

--- window_shade_helper.js
+++ window_shade_helper.js
@@ -6,58 +6,50 @@ var ENDPOINT = 'http://' + config.powerViewIP + '/api/';
 function WindowShadeHelper () {
 };
 
-WindowShadeHelper.prototype.getAllScenes = function() {
+var getAllScenes = function() {
   var options = {
     method: 'GET',
     uri: ENDPOINT + 'scenes?',
-    resolveWithFullResponse: true,
+    resolveWithFullResponse: false,
     json: true
   };
-  return rp(options);
+  var data = rp(options)
+  return data.response.body.sceneData;
 };
 
-WindowShadeHelper.prototype.getAllSceneCollections = function() {
+var getAllSceneCollections = function() {
   var options = {
     method: 'GET',
     uri: ENDPOINT + 'scenecollections?',
     resolveWithFullResponse: true,
     json: true
   };
-  return rp(options);
+  var data = rp(options)
+  return data.response.body.sceneCollectionData;
 };
 
 WindowShadeHelper.prototype.getScene = function(sceneName) {
-  var scenes = this.getAllScenes().sceneData;
+  var scenes = getAllScenes();
   var sceneNameDecoded = '';
-  var sceneId = '';
+  var query = '';
   scenes.forEach(function(scene) {
     sceneNameDecoded = Buffer(scene.name, 'base64').toString('ascii');
     if(sceneName == sceneNameDecoded) {
-      sceneId = scene.id;
+      query = 'scenes?sceneid=' + scene.id;
     }
   });
   if(sceneId == '') {
-    scenes = this.getAllSceneCollections().sceneCollectionData;
+    scenes = getAllSceneCollections().sceneCollectionData;
     scenes.forEach(function(scene) {
       sceneNameDecoded = Buffer(scene.name, 'base64').toString('ascii');
       if(sceneName == sceneNameDecoded) {
-        sceneId = scene.id;
+        query = 'scenecollections?scenecollectionid=' + scene.id;
       }
     });
   }
   var options = {
     method: 'GET',
-    uri: ENDPOINT + 'scenes?sceneid=' + sceneId,
-    resolveWithFullResponse: true,
-    json: true
-  };
-  return rp(options);
-};
-
-WindowShadeHelper.prototype.getSceneCollection = function(sceneId) {
-  var options = {
-    method: 'GET',
-    uri: ENDPOINT + 'scenecollections?scenecollectionid=' + sceneId,
+    uri: ENDPOINT + query,
     resolveWithFullResponse: true,
     json: true
   };
