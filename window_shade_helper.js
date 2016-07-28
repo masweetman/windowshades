5d691c8dc3bf4e1544028a69b2c0fb5edb9f3d5f
Mike Sweetman <mike@michaelsweetman.com>
2016-07-28 05:07:06 +0000

    working test

--- window_shade_helper.js
+++ window_shade_helper.js
@@ -3,36 +3,32 @@ var config = require('./config');
 var rp = require('request-promise');
 var ENDPOINT = 'http://' + config.powerViewIP + '/api/';
 
-function WindowShadeHelper () {
-};
-
-WindowShadeHelper.prototype.getAllScenes = function() {
-  var options = {
-    method: 'GET',
-    uri: ENDPOINT + 'scenes?',
-    resolveWithFullResponse: false,
-    json: true
-  };
-  return rp(options)
+function WindowShadeHelper() {
 };
 
 WindowShadeHelper.prototype.getScene = function(sceneName) {
-  var scenes = []
-  var sceneNameDecoded = '';
-  var query = '';
-  scenes.forEach(function(scene) {
-    sceneNameDecoded = Buffer(scene.name, 'base64').toString('ascii');
-    if(sceneName == sceneNameDecoded) {
-      query = 'scenes?sceneid=' + scene.id;
-    }
-  });
   var options = {
     method: 'GET',
-    uri: ENDPOINT + query,
-    resolveWithFullResponse: true,
+    uri: ENDPOINT + 'scenes?',
     json: true
   };
-  return rp(options);
+  rp(options).then(function (body) {
+      var scenes = body.sceneData;
+      var sceneNameDecoded = '';
+      var query = '';
+      scenes.forEach(function(scene) {
+        sceneNameDecoded = Buffer(scene.name, 'base64').toString('ascii');
+        if(sceneName == sceneNameDecoded) {
+          query = 'scenes?sceneid=' + scene.id;
+        }
+      });
+      options = {
+        method: 'GET',
+        uri: ENDPOINT + query,
+        json: true
+      };
+      rp(options);
+   });
 };
 
 module.exports = WindowShadeHelper;
