37ca99163122a8cdbcc1d48e0675bff1ec71263f
Mike Sweetman <mi894527@bmrn.com>
2016-07-28 10:49:52 -0700

    test

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var WindowShadeHelper = require ('./window_shade_helper');
 var app = new alexa.app('windowshades');
 
 app.launch(function(req,res) {
-  var prompt = 'Tell me which scene you\'d like.';
+  var prompt = 'Tell me to move a shade or recall a scene.';
   res.say(prompt).reprompt(prompt).shouldEndSession(false);
 });
 
@@ -15,7 +15,7 @@ app.intent('sceneintent', {
   'slots': {
     'SCENE': 'NAME'
   },
-  'utterances': ['{|open|close} {-|SCENE}']
+  'utterances': ['{scene|recall} {-|SCENE}']
 },
   function(req, res) {
     var sceneName = req.slot('SCENE');
@@ -27,6 +27,29 @@ app.intent('sceneintent', {
     } else {
       var shadeHelper = new WindowShadeHelper();
       shadeHelper.getScene(sceneName); 
+      res.say('OK.').send();
+    }
+  }
+);
+
+app.intent('positionintent', {
+  'slots': {
+    'SHADE': 'SHADENAME',
+	'POSITION': 'AMAZON.NUMBER',
+  },
+  'utterances': ['{|open|close|set|move|position} {-|SHADE} {|to|at|for} {-|POSITION} {|percent}']
+},
+  function(req, res) {
+    var shadeName = req.slot('SHADE');
+	var shadePosition = Integer.parseInt(req.slot('POSITION'));
+    var reprompt = 'Tell me a shade name and a position percentage.';
+    if (_.isEmpty(shadeName) or _.isEmpty(shadePosition)) {
+      var prompt = 'I didn\'t catch that? Tell me a shade name and a position percentage.';
+      res.say(prompt).reprompt(reprompt).shouldEndSession(false);
+      return true;
+    } else {
+      var shadeHelper = new WindowShadeHelper();
+      shadeHelper.setShadePosition(shadeName, shadePosition); 
       res.say('OK.').send();
     }
   }
--- window_shade_helper.js
+++ window_shade_helper.js
@@ -31,4 +31,31 @@ WindowShadeHelper.prototype.getScene = function(sceneName) {
    });
 };
 
+WindowShadeHelper.prototype.setShadePosition = function(shadeName, shadePosition) {
+  var options = {
+    method: 'GET',
+    uri: ENDPOINT + 'shades?',
+    json: true
+  };
+  rp(options).then(function (body) {
+      var shades = body.shadeData;
+      var shadeNameDecoded = '';
+      var query = '';
+	  var shadePositionValue = MATH.round(65535 * shadePosition / 100);
+      shades.forEach(function(shade) {
+        shadeNameDecoded = Buffer(shade.name, 'base64').toString('ascii');
+        if(shadeName == shadeNameDecoded) {
+          query = 'shades/' + shade.id;
+        }
+      });
+      options = {
+        method: 'PUT',
+        uri: ENDPOINT + query,
+        json: true,
+		position1: shadePositionValue
+      };
+      rp(options);
+   });
+};
+
 module.exports = WindowShadeHelper;
